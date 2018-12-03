fec6fec19b27b1fe787fabf4b2da1905ef29ec55
Patrik Modesto <patrik.modesto@gmail.com>
2018-12-03 19:36:58 +0100

    puzzle_01 finished

--- puzzle_01_2.pl
+++ puzzle_01_2.pl
@@ -1,13 +1,25 @@
 #!/usr/bin/perl
 use warnings;
+use strict;
+
+my @a = ();
+
+while(<>) {
+    chomp;
+    push(@a, $_);
+}
 
 my $i = 0;
 my %x = ();
 
-while(<>) {
-    chomp;
-    $i += $_;
-    last if exists $x{$i};
-    $x[$i] = 1;
+while(1) {
+    foreach my $j (@a) {
+        $i += $j;
+        print "$i\t$j\n";
+        if(defined $x{$i}) {
+            print("Result: $i\n");
+            return;
+        }
+        $x{$i} = $i;
+    }
 }
-print "result: ", $i,"\n";
